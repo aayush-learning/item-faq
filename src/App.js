9ce3dd26a4138642b049c529dd92ea474d35327a
Aayush <chimple.aayush@gmail.com>
2021-01-08 01:04:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+      Game Chest Items will not be available from 10th January 2021
+      </h1>
+      <ul>
+        <li><h2>Will I be able to unlock more items?</h2><h3>You will not be able to unlock any item from now. Don’t worry! We are working on more exciting features for you.</h3></li>
+        <li><h2>What will happen to the items I have already unlocked?</h2><h3>Unfortunately you will not be able to use unlocked items in games.</h3></li>
+        <li> <h2>When are Game Chest Items getting discontinued?</h2><h3>We are discontinuing Game Chest Items from 10th January 2021.</h3></li>
+     
+      </ul>
     </div>
   );
 }
